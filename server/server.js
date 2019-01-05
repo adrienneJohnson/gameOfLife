b0e29b0bf514e5a6b33db58de2543acbadd630c5
Adrienne Johnson <adriennetjohnson@gmail.com>
2019-01-05 15:13:09 -0500

    Fix file serving for deploy

--- server/server.js
+++ server/server.js
@@ -8,10 +8,10 @@ app.use(morgan('dev'));
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.get('*', function (req, res, next) {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'));
 });
 
 const port = process.env.PORT || 3000;
