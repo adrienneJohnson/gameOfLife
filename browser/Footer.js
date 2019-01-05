46035ff809397b6b9b51b23b0a24e48d70940821
Adrienne Johnson <adriennetjohnson@gmail.com>
2019-01-05 15:43:21 -0500

    Fix - footer link

--- browser/Footer.js
+++ browser/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
 	return (
     <footer id="control_panel" className="footer">
       <div className="container">
-        <p className="text-muted">Built by <a>Adrienne Johnson</a></p>
+        <p className="text-muted">Built by <a href="https://www.linkedin.com/in/adrienne-johnson">Adrienne Johnson</a></p>
       </div>
     </footer>
 	)
